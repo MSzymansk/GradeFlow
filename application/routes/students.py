2086d88f9dc9a80669ec06ebd33b90377cbf22bb
MSzymansk <michal76ers@yahoo.com>
2025-05-20 22:28:34 +0200

    Dodanie endpointu POST /students/add

--- application/routes/students.py
+++ application/routes/students.py
@@ -1,20 +1,42 @@
 from flask import Blueprint, jsonify, request, redirect, url_for
+
+from application.database.models import Student
 from application.services import grades_service
 from application.services import student_service
 
 from application.extensions import db
 from flask import render_template
 
-students_bp = Blueprint("students", __name__,url_prefix="/students")
+from application.services.student_service import add_student_to_db
+
+students_bp = Blueprint("students", __name__, url_prefix="/students")
+
 
 @students_bp.route("/", methods=["GET"])
 def get_all():
     session = db.session
     students = student_service.get_all_students(session)
-    return render_template("students.html",students=students)
+    return render_template("students.html", students=students)
+
 
 @students_bp.route("/<int:id>/grades", methods=["GET"])
 def get_class_grades(id):
     session = db.session
-    class_grades = grades_service.get_class_grade(session,id)
+    class_grades = grades_service.get_class_grade(session, id)
     return jsonify(class_grades)
+
+
+@students_bp.route("/add", methods=["POST"])
+def add_student():
+    json = request.get_json()
+    try:
+        new_student = Student(
+            name=json['name'],
+            surname=json['surname'],
+            pesel=json['pesel'],
+            class_id=int(json['class_id'])
+        )
+        return add_student_to_db(db.session, new_student)
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
