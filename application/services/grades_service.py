07d40fa45ee3c0ab43284f5d304cee74ea0d7f3d
MSzymansk <michal76ers@yahoo.com>
2025-05-28 22:19:03 +0200

    small fixes

--- application/services/grades_service.py
+++ application/services/grades_service.py
@@ -20,31 +20,25 @@ def get_class_grade(db_session, class_id: int):
 
 
 def get_all_grades_students_classes(db_session):
-    grades = db_session.query(Grade).join(Student).join(_Class).filter(_Class.teacher_id == session["teacher_id"]).all()
-
+    students_query = db_session.query(Student).join(_Class).filter(_Class.teacher_id == session["teacher_id"]).all()
     students = {}
+    for student in students_query:
+        students[student.id] = {
+            "student_id": student.id,
+            "student_name": student.name,
+            "student_surname": student.surname,
+            "class_name": student._class.name,
+            "class_id": student._class.id,
+            "grades_list": []
+        }
+        if hasattr(student, 'grades'):
+            students[student.id]["grades_list"] = [grade.value for grade in student.grades]
 
-    for grade in grades:
-        student = grade.student
-        student_id = student.id
-
-        if student_id not in students:
-            students[student_id] = {
-                "student_id": student.id,
-                "student_name": student.name,
-                "student_surname": student.surname,
-                "class_name": student._class.name,
-                "class_id": student._class.id,
-                "grades_list": []
-            }
-
-        students[student_id]["grades_list"].append(grade.value)
-
-    for student_data in students.values():
-        grades_list = student_data["grades_list"]
-        student_data["avg_grade"] = sum(grades_list) / len(grades_list) if grades_list else 0
+        grades_list = students[student.id]["grades_list"]
+        students[student.id]["avg_grade"] = sum(grades_list) / len(grades_list) if grades_list else 0
 
     return list(students.values())
+
 
 
 def get_students_grades(db_session, id: int):
